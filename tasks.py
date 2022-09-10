2e51c527bded94c114dcc01cec92fba5194809ad
Riku Rauhala <riku@rauhala.io>
2022-09-10 17:28:35 +0300

    Create the initial project structure

---
+++ tasks.py
@@ -0,0 +1,31 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py")
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
+
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
+
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src")
