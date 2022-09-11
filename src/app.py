eaf53b875ce6c27f7742b36a39b2fef5185cab9c
Riku Rauhala <riku@rauhala.io>
2022-09-11 18:46:46 +0300

    Create keygen.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from functions.keygen import KeyGenerator
+
 from console_io import ConsoleIO
 
 COMMANDS = {
@@ -11,19 +13,19 @@ COMMANDS = {
 class Application:
     def __init__(self):
         self._io = ConsoleIO()
+        self._key_generator = KeyGenerator()
 
     def start(self):
         self._io.print("Application\n")
         self._print_commands()
 
         while True:
-            command = self._io.read("command: ")
-
+            command = self._io.read("Enter command: ")
             match command:
                 case "q":
                     break
                 case "1":
-                    self._generate_keys()
+                    self._key_generator.generate_keys()
                 case _:
                     self._io.print("Invalid command!")
                     self._print_commands()
@@ -32,6 +34,3 @@ class Application:
         self._io.print("Commands:")
         for command in COMMANDS:
             self._io.print(COMMANDS[command])
-
-    def _generate_keys(self):
-        pass
---
+++ src/functions/keygen.py
@@ -0,0 +1,13 @@
+class KeyGenerator:
+    def __init__(self):
+        pass
+
+    def generate_keys(self):
+        self._generate_public_key()
+        self._generate_private_key()
+
+    def _generate_public_key(self):
+        print("Generating public key")
+
+    def _generate_private_key(self):
+        print("Generating private key")
