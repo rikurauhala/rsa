8b3ef8621672ef9f8f9b09d0842902d1c1904ddc
Riku Rauhala <riku@rauhala.io>
2022-10-01 20:30:32 +0300

    Add crypt_test.py

---
+++ src/tests/functions/crypt_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+from functions.crypt import Crypt
+from functions.keygen import KeyGenerator
+
+
+class TestCrypt(unittest.TestCase):
+    def setUp(self):
+        self._crypt = Crypt()
+        self._key_generator = KeyGenerator()
+        self._keys = self._key_generator.generate_keys()
+        self._message_m = "Hello, world!"
+        self._message_c = -1
+
+    def test_encryption_returns_integer(self):
+        self._message_c = self._crypt.encrypt(
+            self._message_m,
+            self._keys["e"],
+            self._keys["n"]
+        )
+        type_of_message_c = type(self._message_c)
+        self.assertEqual(type_of_message_c, int)
+    
+    def test_decryption_returns_string(self):
+        message = self._crypt.decrypt(
+            self._message_c,
+            self._keys["d"],
+            self._keys["n"]
+        )
+        type_of_message = type(message)
+        self.assertEqual(type_of_message, str)
