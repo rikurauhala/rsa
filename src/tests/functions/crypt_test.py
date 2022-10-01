060df9b9a512170d7d9ecc70229caff6314d9f9d
Riku Rauhala <riku@rauhala.io>
2022-10-01 21:06:42 +0300

    Improve crypt_test.py

--- src/tests/functions/crypt_test.py
+++ src/tests/functions/crypt_test.py
@@ -10,22 +10,16 @@ class TestCrypt(unittest.TestCase):
         self._key_generator = KeyGenerator()
         self._keys = self._key_generator.generate_keys()
         self._message_m = "Hello, world!"
-        self._message_c = -1
+        self._message_c = self._crypt.encrypt(self._message_m, self._keys["e"], self._keys["n"])
+        self._message_p = self._crypt.decrypt(self._message_c, self._keys["d"], self._keys["n"])
 
     def test_encryption_returns_integer(self):
-        self._message_c = self._crypt.encrypt(
-            self._message_m,
-            self._keys["e"],
-            self._keys["n"]
-        )
         type_of_message_c = type(self._message_c)
         self.assertEqual(type_of_message_c, int)
     
     def test_decryption_returns_string(self):
-        message = self._crypt.decrypt(
-            self._message_c,
-            self._keys["d"],
-            self._keys["n"]
-        )
-        type_of_message = type(message)
+        type_of_message = type(self._message_p)
         self.assertEqual(type_of_message, str)
+
+    def test_decryption_succeeds(self):
+        self.assertEqual(self._message_p, self._message_m)
