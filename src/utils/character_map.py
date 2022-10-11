49fefeb93101e468473f9d91b62ab16a2a814157
Riku Rauhala <riku@rauhala.io>
2022-10-11 10:32:14 +0300

    Fix linter issues in character_map.py

--- src/utils/character_map.py
+++ src/utils/character_map.py
@@ -3,36 +3,36 @@ from utils.dictionary import CHARACTERS
 
 class CharacterMap:
     """Turns characters into integers and integers into characters.
-    
+
     Implements a custom data structure to map characters to order numbers
     and vice versa. See the documentation for a list of supported characters.
     """
 
     def __init__(self):
         """Initializes a new CharacterMap."""
-        self._CHARACTERS = CHARACTERS
-        self._ORDER = {chr: ord for ord, chr in CHARACTERS.items()}
+        self._characters = CHARACTERS
+        self._order = {chr: ord for ord, chr in CHARACTERS.items()}
 
-    def get_chr(self, ord):
+    def get_chr(self, order):
         """Turns a character into an integer.
 
         Args:
-            ord (integer): An integer with a value between 10 and 99.
+            order (integer): An integer with a value between 10 and 99.
 
         Returns:
-            chr (character): A character.
+            char (character): A character.
         """
-        chr = self._CHARACTERS[ord]
-        return chr
+        char = self._characters[order]
+        return char
 
-    def get_ord(self, chr):
+    def get_ord(self, char):
         """Turns an integer into a character.
 
         Args:
-            chr (character): A character.
+            char (character): A character.
 
         Returns:
-            ord (integer): An integer with a value between 10 and 99.
+            order (integer): An integer with a value between 10 and 99.
         """
-        ord = self._ORDER[chr]
-        return ord
+        order = self._order[char]
+        return order
