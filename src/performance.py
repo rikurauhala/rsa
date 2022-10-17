02df70af22e0e6d48cc997a32d913a1e99be48c5
Riku Rauhala <riku@rauhala.io>
2022-10-17 19:05:47 +0300

    Test decryption performance

--- src/performance.py
+++ src/performance.py
@@ -23,3 +23,14 @@ def test_rsa_encryption(benchmark):
     (public_key, private_key) = rsa.newkeys(key_length)
     message_encoded = message.encode('utf8')
     benchmark(rsa.encrypt, message_encoded, public_key)
+
+def test_own_decryption(benchmark):
+    keys = key_generator.generate_keys()
+    ciphertext = crypt.encrypt(message, keys["e"], keys["n"])
+    benchmark(crypt.decrypt, ciphertext, keys["d"], keys["n"])
+
+def test_rsa_decryption(benchmark):
+    (public_key, private_key) = rsa.newkeys(key_length)
+    message_encoded = message.encode('utf8')
+    ciphertext = rsa.encrypt(message_encoded, public_key)
+    benchmark(rsa.decrypt, ciphertext, private_key)
